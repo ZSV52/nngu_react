8e699c8ee4dc38b7ffee79bbf919395bbb43e092
ZSV52 <mixgus88@gmail.com>
2022-05-07 11:25:39 +0300

    upd

---
+++ src/components/ItemsList/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ItemsList = () => {
+  return (
+    <div>
+      <p>Список товаров</p>
+      <div>Тут будет список товаров</div>
+    </div>
+  );
+};
+
+export default ItemsList;
--- src/components/MainPage/index.tsx
+++ src/components/MainPage/index.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import Header from '../Header';
+import ItemsList from '../ItemsList';
 import style from './MainPage.module.scss';
 
 const MainPage = () => {
-  const isTrue = false;
-  const text1 = 'text1';
-  const text2 = 'text2';
+  // =============================================
 
-  return <div className={style.main_page}>{isTrue ? text1 : text2}</div>;
+  //   const isTrue = false;
+  //   const text1 = 'text1';
+  //   const text2 = 'text2';
+
+  //   return <div className={style.main_page}>{isTrue ? text1 : text2}</div>;
+
+  // =============================================
+
+  return (
+    <>
+      <Header />
+      <ItemsList />;
+    </>
+  );
 };
 
 export default MainPage;
